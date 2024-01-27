35fd2bcdbc46f3615ec27fce1e03ed1211028b11
Sunil Pattanaik <sunilpattanaik@Sunils-MacBook-Air.local>
2024-01-27 22:07:42 +0800

    added

---
+++ src/main/java/com/docker/demo/SpringBootDockerDemoApplication.java
@@ -0,0 +1,22 @@
+package com.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerDemoApplication {
+	
+	@GetMapping("/message")
+	public String getMessage()
+	{
+		return "Welcome to Docker";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerDemoApplication.class, args);
+	}
+
+}
